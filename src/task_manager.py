82bde6e664edba3c6d572d5165f02009436c88d6
Pempho-Mackson-Kapulula <pemphomacksonkapulula@gmail.com>
2026-05-20 10:24:37 +0200

    D:add task manager module with methods to load and save tasks and add defaults to json file fileds

---
+++ src/task_manager.py
@@ -0,0 +1,44 @@
+import json
+
+
+JSON_FILE = "todos.json"
+
+PRIORITY_VALUES = {"High" : 1, "MEdium" : 2, "Low":3}
+
+
+class TaskManager:
+    def __init__(self, json_file=JSON_FILE):
+        self.json_file = JSON_FILE
+        self.tasks = self.load_tasks()
+        
+    def  load_tasks(self):
+        if not os.path.exists(self.json_file):
+            return []
+        
+        with open(self.json_file, mode = "rt") as f:
+            data = f.read().strip()
+            if not data:
+                return []
+            tasks = json.loads(data)
+            
+        for task in tasks:
+            if "task_description" not in task:
+                task["task_description"] = ""
+            if "task_name" not in task:
+                task["task_name"] = ""
+            if "category" not in task:
+                task["category"] = "Personal"
+            if "priority" not in task:
+                task["priority"] = "Medium"
+            if "deadline" not in task:
+                task["deadline"] = ""
+            if "done" not in task:
+                task["done"] = False
+            if "xp_awarded" not in task:
+                task["xp_awarded"] = False
+         
+        return tasks
+    
+    def save_tasks(self):
+        with open(self.json_file, mode = "w") as f:
+            json.dump(self.tasks,f,indent=5)    
